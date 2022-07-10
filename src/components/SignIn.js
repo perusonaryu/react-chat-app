ae85b08b32ab619d8cd37c8fea7e657d5bc67e34
ryusei <perusonaryu1127@gmail.com>
2022-07-10 22:44:27 +0900

    set name bu button click

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -75,12 +75,15 @@ export default function SignIn({setName}) {
           />
 
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
             disabled={disabled}
+            onClick={() => {
+                setName(string);
+            }}
           >
             はじめる
           </Button>
